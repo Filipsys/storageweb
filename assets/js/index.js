8c237851841fb2b7185e2854c4c19508c2f893cf
Filipsys <filipsysak@gmail.com>
2024-05-31 23:21:22 +0200

    end of day commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,19 +9,70 @@ let mouseY = 0;
 let velocityX = 0;
 let velocityY = 0;
 
+let shapeIsDragging = false;
+
+
 // Functions
+
+function showActiveShapeSelected(shapeId) {
+    shapesData[shapeId].isSelected = true;
+}
+
+function hideActiveShapeSelected(shapeId) {
+    shapesData[shapeId].isSelected = false;
+}
+
 
 function createShape(shapeId, x, y, width, height, color) {
     const shape = document.createElement("div");
+    shape.classList.add("note-box");
     shape.style.position = "absolute";
     shape.style.left = x + "px";
     shape.style.top = y + "px";
-    shape.style.width = width + "px";
-    shape.style.height = height + "px";
-    shape.style.backgroundColor = color;
+    // shape.style.width = width + "px";
+    // shape.style.height = height + "px";
+    // shape.style.backgroundColor = color;
+    shape.style.border = `3px solid ${color}`;
     shape.id = shapeId;
 
     app.appendChild(shape);
+
+
+    shape.addEventListener("mousedown", (event) => {
+        shapeIsDragging = true;
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+
+        showActiveShapeSelected(shapeId);
+    });
+
+    shape.addEventListener("mouseup", (event) => {
+        shapeIsDragging = false;
+
+        hideActiveShapeSelected(shapeId);
+    });
+
+    shape.addEventListener("mouseleave", (event) => {
+        shapeIsDragging = false;
+
+        hideActiveShapeSelected(shapeId);
+    });
+
+    shape.addEventListener("mousemove", (event) => {
+        if (shapeIsDragging) {
+            const shapeX = shapesData[shapeId].x;
+            const shapeY = shapesData[shapeId].y;
+
+            const newShapeX = shapeX + event.clientX - mouseX;
+            const newShapeY = shapeY + event.clientY - mouseY;
+
+            shapesData[shapeId].x = newShapeX;
+            shapesData[shapeId].y = newShapeY;
+
+            mouseX = event.clientX;
+            mouseY = event.clientY;
+        }
+    });
 }
 
 function saveShape(shapeId, x, y, width, height, color) {
@@ -30,8 +81,8 @@ function saveShape(shapeId, x, y, width, height, color) {
 
 
 saveShape("shape1", 100, 100, 100, 100, "#ff0000");
-saveShape("shape2", 200, 200, 100, 100, "#00ff00");
-saveShape("shape3", 300, 300, 100, 100, "#0000ff");
+saveShape("shape2", 400, 200, 100, 100, "#00ff00");
+saveShape("shape3", 200, 700, 100, 100, "#0000ff");
 
 // for (const shapeId in shapesData) {
 //     createShape(shapeId, shapesData[shapeId].x, shapesData[shapeId].y, shapesData[shapeId].width, shapesData[shapeId].height, shapesData[shapeId].color);
@@ -62,8 +113,8 @@ app.addEventListener("mousemove", (event) => {
         mouseX = event.clientX;
         mouseY = event.clientY;
 
-        velocityX = deltaX * 0.3;
-        velocityY = deltaY * 0.3;
+        velocityX = deltaX * 0.6;
+        velocityY = deltaY * 0.6;
     }
 });
 
@@ -73,19 +124,17 @@ app.addEventListener("mousemove", (event) => {
 function animate() {
     const render = () => {
         app.innerHTML = "";
-    
+
         for (const shapeId in shapesData) {
             const { x, y, width, height, color } = shapesData[shapeId];
-    
-            if (x + width > window.innerWidth) {
-                x = window.innerWidth - width;
-            }
-    
-            if (y + height > window.innerHeight) {
-                y = window.innerHeight - height;
-            }
-    
+
             createShape(shapeId, x, y, width, height, color);
+
+            if (shapesData[shapeId].isSelected) {
+                const selectedShape = document.getElementById(shapeId);
+
+                selectedShape.style.transform = `scale(1.1)`;
+            }
         }
     };
 
