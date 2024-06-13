5ca0f3a56e6c3d36d06e3a4175a43fc5d7dab64e
Filipsys <filipsysak@gmail.com>
2024-06-13 23:12:21 +0200

    Fixing saving into DB

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
 // import { main } from "bun";
 
-const app = document.getElementById("app");
-
 let shapesData = {};
 let shapeIsDragging = false;
 let selectedShapeScale = 1;
@@ -43,7 +41,8 @@ function createShape(shapeId, x, y, width, height, dataType, data, color) {
         shape.innerHTML = data;
     }
 
-    app.appendChild(shape);
+    const appElement = document.getElementById("app");
+    appElement.appendChild(shape);
 
 
     // Mouse events for the shape
@@ -88,7 +87,7 @@ function createShape(shapeId, x, y, width, height, dataType, data, color) {
     });
 }
 
-function createShape(x, y, width, height, dataType = "text", dataLink = null, data = null, color) {
+function saveShape(x, y, width, height, dataType = "text", dataLink = null, data = null, color) {
     // shapesData[shapeId] = { x, y, width, height, color };
 
     console.log(x, y, width, height, dataType, dataLink, data, color);
@@ -152,29 +151,52 @@ app.addEventListener("wheel", (event) => {
 
 // Animation & render loop
 
-function animate() {
+
+function showAllShapes(shapeData) {
+    for (const shape in shapeData) {
+        let currentShape = shapeData[shape];
+        
+        switch (currentShape.dataType) {
+            case "link":
+                createShape(x = currentShape.x, y = currentShape.y, width = currentShape.width, height = currentShape.height, dataType = "link", dataLink = currentShape.dataLink, data = null, color = currentShape.color, id = currentShape.id);
+                break;
+            case "image": {
+                createShape(x = currentShape.x, y = currentShape.y, width = currentShape.width, height = currentShape.height, dataType = "image", dataLink = currentShape.dataLink, data = null, color = currentShape.color);
+                break;
+            }
+            case "text": {
+                createShape(x = currentShape.x, y = currentShape.y, width = currentShape.width, height = currentShape.height, dataType = "text", dataLink = null, data = currentShape.data, color = currentShape.color);
+                break;
+            }
+        }
+    }
+}
+
+function animate(shapeData) {
+    const savedData = shapeData;
+
     const render = () => {
         app.innerHTML = "";
 
-        for (const shapeId in shapesData) {
+        showAllShapes(savedData);
+
+        // for (const shapeId in shapesData) {
             // const { x, y, width, height, color } = shapesData[shapeId];
 
-            // createShape(x = 100, y = 100, width = 100, height = 100, dataType = "text", dataLink = null, data = "Hello World", color = "#ff0000");
+            // if (shapesData[shapeId].isSelected) {
+            //     const selectedShape = document.getElementById(shapeId);
 
-            if (shapesData[shapeId].isSelected) {
-                const selectedShape = document.getElementById(shapeId);
+            //     const interval = setInterval(() => {
+            //         if (selectedShapeScale < 1.1) {
+            //             selectedShapeScale += 0.01;
+            //         } else {
+            //             clearInterval(interval);
+            //         }
+            //     }, 100);
 
-                const interval = setInterval(() => {
-                    if (selectedShapeScale < 1.1) {
-                        selectedShapeScale += 0.01;
-                    } else {
-                        clearInterval(interval);
-                    }
-                }, 100);
-
-                selectedShape.style.scale = selectedShapeScale;
-            }
-        }
+            //     selectedShape.style.scale = selectedShapeScale;
+            // }
+        // }
     };
 
     requestAnimationFrame(animate);
@@ -353,13 +375,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 switch (btnsList.indexOf(btn)) {
                     case 0:
-                        createShape(x = 100, y = 100, width = 100, height = 100, dataType = "link", dataLink = "https://www.google.com", data = null, color = "#ff0000");
+                        saveShape(x = 100, y = 100, width = 100, height = 100, dataType = "link", dataLink = "https://www.google.com", data = null, color = "#ff0000");
+                        console.log("link");
                         break;
                     case 1:
-                        createShape(x = 100, y = 100, width = 100, height = 100, dataType = "image", dataLink = "https://i.imgur.com/y1h0hKc.png", data = null, color = "#ff0000");
+                        saveShape(x = 100, y = 100, width = 100, height = 100, dataType = "image", dataLink = "https://i.imgur.com/y1h0hKc.png", data = null, color = "#ff0000");
+                        console.log("image");
                         break;
                     case 2:
-                        createShape(x = 100, y = 100, width = 100, height = 100, dataType = "text", dataLink = null, data = "Hello World", color = "#ff0000");
+                        saveShape(x = 100, y = 100, width = 100, height = 100, dataType = "text", dataLink = null, data = "Hello World", color = "#ff0000");
+                        console.log("text");
                         break;
                 }
             });
@@ -368,46 +393,12 @@ document.addEventListener("DOMContentLoaded", () => {
         shapeBg.appendChild(shapePopup);
     });
 
-
     axios.get("http://localhost:3000/api/load").then((response) => {
-        const responseData = response.data;
-
-        console.log(responseData);
-
-        // [
-        //     {
-        //         "id": 2,
-        //         "x": 100,
-        //         "y": 100,
-        //         "width": 100,
-        //         "height": 100,
-        //         "dataType": "text",
-        //         "dataLink": null,
-        //         "data": "Hello World",
-        //         "color": "#ff0000"
-        //     }
-        // ]
-
-        for (let i = 0; i < responseData.length; i++) {
-            const shape = responseData[i];
-
-            switch (shape.dataType) {
-                case "link":
-                    createShape(x = shape.x, y = shape.y, width = shape.width, height = shape.height, dataType = "link", dataLink = shape.dataLink, data = null, color = shape.color);
-                    break;
-                case "image": {
-                    createShape(x = shape.x, y = shape.y, width = shape.width, height = shape.height, dataType = "image", dataLink = shape.dataLink, data = null, color = shape.color);
-                    break;
-                }
-                case "text": {
-                    createShape(x = shape.x, y = shape.y, width = shape.width, height = shape.height, dataType = "text", dataLink = null, data = shape.data, color = shape.color);
-                    break;
-                }
-            }
-        }
+        const savedData = response.data;
+        console.log(savedData);
+        
+        animate(savedData); 
     });
-
-    animate();
 
     const configSection = document.getElementById("config-section");
     configSection.style.transition = "left 0.1s ease-in-out";
