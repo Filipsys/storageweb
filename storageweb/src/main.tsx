112555f436f6105fe654c02e6480a22900518308
Filipsys <filipsysak@gmail.com>
2024-06-18 22:25:40 +0200

    React code update

---
+++ storageweb/src/main.tsx
@@ -0,0 +1,51 @@
+import { StrictMode, useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Fullscreen, Container } from "@react-three/uikit";
+import "./App.css";
+import "./index.css";
+
+
+async function fetchData() {
+    const response = await fetch("http://127.0.0.1:3000/api/data", { method: "GET", headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET" } });
+
+    return await response.json();
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+function MyComponent(): JSX.Element {
+    const [data, setData] = useState<JSX.Element | null>(null);
+
+    useEffect(() => {
+        fetchData().then((response) => {
+            // setData(<p>{response}</p>);
+
+            setData(response.map((element: Element) => (
+                <div className={`${element.id} note-element`} key={element.id} style={{ position: "absolute", left: element.x, top: element.y, width: element.width, height: element.height, backgroundColor: element.color, border: `3px solid ${element.color}` }}>
+                    {element.dataType === "text" ? <p>{element.data}</p> : null}
+                </div>
+            )));
+
+            interface Element {
+                id: number;
+                x: number;
+                y: number;
+                width: number;
+                height: number;
+                color: string;
+                dataType: string;
+                data: string;
+            }
+        });
+    }, []);
+
+    return <>{data}</>;
+}
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Fullscreen>
+            <MyComponent />
+        </Fullscreen>
+    </StrictMode>
+);
---
+++ storageweb/src/server/server.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import { Database } from "bun:sqlite";
+
+
+const db = new Database("./src/database/storage.sqlite");
+const app = express();
+const port = 3000;
+
+const cors = require("cors");
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            // access-control-allow-credentials:true
+  optionSuccessStatus:200,
+};
+
+app.use(cors(corsOptions));
+// app.use(express.json());
+
+app.get("/api/data", (_request, response) => {
+  let data = db.prepare("SELECT * FROM elements").all();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data = data.map((element: any) => ({
+    id: element.id,
+    x: element.x,
+    y: element.y,
+    width: element.width,
+    height: element.height,
+    dataType: element.dataType,
+    dataLink: element.dataLink,
+    data: element.data,
+    color: element.color,
+  }));
+
+  response.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
