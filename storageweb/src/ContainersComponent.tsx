402e5ebb21d232ffa9d624d874e61c348bb64ac6
Filipsys <filipsysak@gmail.com>
2024-06-22 16:45:08 +0200

    chore: Add resizing functionality to ContainersComponent WIP

--- storageweb/src/ContainersComponent.tsx
+++ storageweb/src/ContainersComponent.tsx
@@ -7,10 +7,55 @@ import DragIndicatorRoundedIcon from "@mui/icons-material/DragIndicatorRounded";
 // =====/=====/=====
 
 
+let resizing = false;
+
+
 interface ContainersComponentProps {
     data: {
         [x: string]: any;
     };
+}
+
+function handleMouseDown(event: React.MouseEvent<HTMLDivElement>) {
+    resizing = true;
+
+    if (resizing) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        // Get the main container (two levels up from the resize handle)
+        const mainContainer = event.currentTarget.parentElement?.parentElement?.parentElement;
+
+        if (mainContainer) {
+            // Get the bounding rectangle of the main container
+            const rect = mainContainer.getBoundingClientRect();
+
+            // Get the x and y position of the main container
+            const containerX = rect.left;
+            const containerY = rect.top;
+
+            // Get the current width and height of the main container
+            const containerWidth = rect.width;
+            const containerHeight = rect.height;
+
+            console.log('Container X:', containerX);
+            console.log('Container Y:', containerY);
+            console.log('Container Width:', containerWidth);
+            console.log('Container Height:', containerHeight);
+
+            // Calculate the new width and height
+            const newWidth = containerWidth + (mouseX - (containerX + containerWidth));
+            const newHeight = containerHeight + (mouseY - (containerY + containerHeight));
+
+            // Set the new width and height of the main container
+            mainContainer.style.width = `${newWidth}px`;
+            mainContainer.style.height = `${newHeight}px`;
+        }
+    }
+}
+
+function handleMouseUp(event: React.MouseEvent<HTMLDivElement>) {
+    resizing = false;
 }
 
 const ContainersComponent: React.FC<ContainersComponentProps> = ({ data }) => {
@@ -25,7 +70,7 @@ const ContainersComponent: React.FC<ContainersComponentProps> = ({ data }) => {
 
                     <Container positionType={"absolute"} positionBottom={10} positionRight={10} width={10} height={10} margin={10}>
                         <Html>
-                            <div style={{ "cursor": "sw-resize" }} onPointerDown={() => console.log('down')} onPointerUp={() => console.log('up')}>
+                            <div style={{ "cursor": "sw-resize" }} onPointerDown={handleMouseDown} onPointerUp={handleMouseUp}>
                                 <DragIndicatorRoundedIcon />
                             </div>
                         </Html>
