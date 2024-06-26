753baa47eabe11e6816222a70ca97c024e91c2e0
Filipsys <filipsysak@gmail.com>
2024-06-26 16:48:02 +0200

    feat: Add AdsClickRoundedIcon to Navbar component

--- storageweb/src/Navbar.tsx
+++ storageweb/src/Navbar.tsx
@@ -1,31 +1,23 @@
 import { Container } from "@react-three/uikit";
 import { Html } from "@react-three/drei";
 import PanToolRoundedIcon from '@mui/icons-material/PanToolRounded';
-
+import AdsClickRoundedIcon from '@mui/icons-material/AdsClickRounded';
 
 // =====/=====/=====/=====
 
 
 
 const Navbar = () => {
-    // const [navbar, setNavbar] = useState<JSX.Element | null>(null);
-
-    // useEffect(() => {
-    //     setNavbar(
-
-    //     );
-    // }, []);
-
     return (
         <Container width={"100%"} height={50} positionType={"absolute"} positionTop={0} positionLeft={0} justifyContent={"center"} alignItems={"center"} marginTop={15}>
             <Container width={"auto"} justifyContent={"space-between"} alignItems={"center"} backgroundColor={"#232329"} borderRadius={10} padding={2} >
                 <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html><PanToolRoundedIcon /></Html></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
-                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html><AdsClickRoundedIcon /></Html></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html></Html></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html></Html></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html></Html></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html></Html></Container>
+                <Container width={40} height={40} borderRadius={5} backgroundColor={"#403e6a"} margin={5}><Html></Html></Container>
             </Container>
         </Container>
     );
