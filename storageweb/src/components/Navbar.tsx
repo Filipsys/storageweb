b369bb83825796951069c5c46d18858a1c2f0173
Filipsys <filipsysak@gmail.com>
2024-06-28 15:59:13 +0200

    refactor: Update Navbar component import path and icons

--- storageweb/src/components/Navbar.tsx
+++ storageweb/src/components/Navbar.tsx
@@ -1,63 +1,89 @@
-import { Container, Svg } from "@react-three/uikit";
-import { Html } from "@react-three/drei";
-import { EditOutlined, DeleteOutlined, InsertPhotoOutlined, InsertDriveFileOutlined } from "@material-ui/icons";
-import { BackHandOutlined, AdsClickOutlined } from "@mui/icons-material";
-import { useState, useCallback } from "react";
+import { Container, Svg, Text } from "@react-three/uikit";
+// import { Html } from "@react-three/drei";
+// import { EditOutlined, DeleteOutlined, InsertPhotoOutlined, InsertDriveFileOutlined } from "@material-ui/icons";
+// import { BackHandOutlined, AdsClickOutlined } from "@mui/icons-material";
+// import { useState, useCallback } from "react";
 
 
 // =====/=====/=====/=====
 
 
 const Navbar = () => {
-    // const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-    // const handleMouseEnter = useCallback((index: number) => {
-    //     setHoveredIndex(index);
-    // }, []);
-
-    // const handleMouseLeave = useCallback(() => {
-    //     setHoveredIndex(null);
-    // }, []);
-
-    const icons = [
-        { icon: <Svg width={16} height={16} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/back_hand/materialsymbolsoutlined/back_hand_wght500_20px.svg" color={"#7f7f7f"} />, backgroundColor: "#232329" },
-        { icon: <Svg width={16} height={16} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/ads_click/materialsymbolsoutlined/ads_click_wght500_20px.svg" color={"#79706c"} />, backgroundColor: "#403e6a" },
-        { icon: <Svg width={16} height={16} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/edit/materialsymbolsoutlined/edit_wght500_20px.svg" color={"#79706c"} />, backgroundColor: "#232329" },
-        { icon: <Svg width={14} height={14} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/delete/materialsymbolsoutlined/delete_wght500_20px.svg" color={"#79706c"} />, backgroundColor: "#232329" },
-        { icon: <Svg width={16} height={16} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/photo/materialsymbolsoutlined/photo_wght500_20px.svg" color={"#79706c"} />, backgroundColor: "#31303b" },
-        { icon: <Svg width={16} height={16} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/drive_file/materialsymbolsoutlined/drive_file_wght500_20px.svg" color={"#79706c"} />, backgroundColor: "#31303b" }
-    ];
-
     return (
         <Container width={"100%"} height={50} positionType={"absolute"} positionTop={0} positionLeft={0} justifyContent={"center"} alignItems={"center"} marginTop={15}>
-            <Container width={"auto"} justifyContent={"space-around"} alignItems={"center"} backgroundColor={"#232329"} borderRadius={10} padding={2}>
-                {icons.map((item, index) => (
-                    <Container
-                        key={index}
-                        width={40}
-                        height={40}
-                        borderRadius={5}
-                        // backgroundColor={hoveredIndex === index ? "#31303b" : item.backgroundColor}
-                        margin={5}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        // onPointerEnter={() => handleMouseEnter(index)}
-                        // onPointerLeave={handleMouseLeave}
-                        hover={{ backgroundColor: "#31303b" }}
-                    >
-                        <Container width={16} height={16} hover={{ backgroundColor: "#31303b" }}>
-                            {/* <Html onPointerEnter={() => handleMouseEnter(index)} onPointerLeave={handleMouseLeave}>
-                                <div style={{ pointerEvents: "none" }}>
-                                    {item.icon}
-                                </div>
-                            </Html> */}
-
-                            {item.icon}
-
-                            {/* <Svg width={20} height={20} src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/back_hand/materialsymbolsoutlined/back_hand_20px.svg" color={"white"} /> */}
-                        </Container>
+            <Container width={"auto"} justifyContent={"space-around"} alignItems={"center"} backgroundColor={"#232329"} borderRadius={10} padding={2} borderTopRightRadius={0} borderBottomRightRadius={0}>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/back_hand/materialsymbolsoutlined/back_hand_20px.svg"
+                            color={"white"} />
                     </Container>
-                ))}
+                </Container>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/edit/materialsymbolsoutlined/edit_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/delete/materialsymbolsoutlined/delete_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/delete/materialsymbolsoutlined/delete_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/delete/materialsymbolsoutlined/delete_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#31303b" }}>
+                    <Container width={16} height={16}>
+                        <Svg width={20} opacity={0.5} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/delete/materialsymbolsoutlined/delete_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+            </Container>
+
+            <Container width={"auto"} justifyContent={"space-around"} alignItems={"center"} backgroundColor={"#2e2e36"} borderRadius={10} borderTopLeftRadius={0} borderBottomLeftRadius={0} padding={2}>
+                <Container width={20} height={40} borderRadius={5} backgroundOpacity={0.5} margin={5} justifyContent={"center"} alignItems={"center"}>
+                    <Text opacity={0.3} fontSize={24} color={"white"} width={20} textAlign={"center"} verticalAlign={"middle"}>+</Text>
+                </Container>
+
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#1d1d22" }}>
+                    <Container width={16} height={16}>
+                        <Svg opacity={0.5} width={20} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/link/materialsymbolsoutlined/link_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#1d1d22" }}>
+                    <Container width={16} height={16}>
+                        <Svg opacity={0.5} width={20} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/add_photo_alternate/materialsymbolsoutlined/add_photo_alternate_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
+
+                <Container width={40} height={40} borderRadius={5} backgroundOpacity={0.5} backgroundColor={"#232329"} margin={5} justifyContent={"center"} alignItems={"center"} hover={{ backgroundColor: "#1d1d22" }}>
+                    <Container width={16} height={16}>
+                        <Svg opacity={0.5} width={20} height={20}
+                            src="https://raw.githubusercontent.com/google/material-design-icons/40811b1dea7c91e4221d353a35c10490b93e8778/symbols/web/ads_click/materialsymbolsoutlined/ads_click_wght500_20px.svg"
+                            color={"white"} />
+                    </Container>
+                </Container>
             </Container>
         </Container>
     );
