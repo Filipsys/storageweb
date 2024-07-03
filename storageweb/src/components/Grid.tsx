fcef0a18bb18fce504ef48ce908f7ea3e5e20368
Filipsys <filipsysak@gmail.com>
2024-07-03 17:09:56 +0200

    refactor: Update Grid component to display grid overlay

--- storageweb/src/components/Grid.tsx
+++ storageweb/src/components/Grid.tsx
@@ -1,17 +1,26 @@
 import { Container, Svg } from "@react-three/uikit";
-// import { Html } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 // import { EditOutlined, DeleteOutlined, InsertPhotoOutlined, InsertDriveFileOutlined } from "@material-ui/icons";
 // import { BackHandOutlined, AdsClickOutlined } from "@mui/icons-material";
 
 const Grid = () => {
     return (
-        <Container positionType={"absolute"} positionLeft={"-10%"} positionTop={"-10%"} width={"120%"} height={"120%"} zIndexOffset={-100} backgroundColor={"blue"}>
-            {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-20%"} /> */}
-            {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-20%"} /> */}
-            {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-40%"} /> */}
-            {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-40%"} /> */}
+        <Container positionType={"relative"} width={"100%"} height={"100%"} backgroundColor={"blue"}>
+            <Html>
+                <div style={{
+                    width: "200vw",
+                    height: "100%",
+                    backgroundImage: 
+                }}>
+
+                </div>
+            </Html>
         </Container>
     );
 }
+{/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-20%"} /> */}
+{/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-20%"} /> */}
+{/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-40%"} /> */}
+{/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-40%"} /> */}
 
 export default Grid;
