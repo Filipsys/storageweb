bc1903c025dd21a90e43d0d250ed490edfb7d6de
Filipsys <filipsysak@gmail.com>
2024-07-04 23:09:25 +0200

    refactor: Update Grid component styles and remove commented out code

--- storageweb/src/components/Grid.tsx
+++ storageweb/src/components/Grid.tsx
@@ -1,12 +1,10 @@
-import { Container, Svg } from "@react-three/uikit";
+import { Container } from "@react-three/uikit";
 import { Html } from "@react-three/drei";
-// import { EditOutlined, DeleteOutlined, InsertPhotoOutlined, InsertDriveFileOutlined } from "@material-ui/icons";
-// import { BackHandOutlined, AdsClickOutlined } from "@mui/icons-material";
 
 const Grid = () => {
     return (
         <Container width={"100%"} height={"100%"}>
-            {/* <Html>
+            <Html>
                 <div style={{
                     width: '100vw',
                     height: '100vh',
@@ -14,14 +12,16 @@ const Grid = () => {
                     backgroundSize: '30px 30px',
                     }}>
                 </div>
-            </Html> */}
+            </Html>
         </Container>
     );
 }
+
+export default Grid;
+
+
 
 {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-20%"} /> */}
 {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-20%"} /> */}
 {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20%"} positionTop={"-40%"} /> */}
 {/* <Svg src={"http://localhost:3000/assets?path=grid.svg"} width={"60%"} height={"60%"} color={"white"} opacity={0.2} positionLeft={"-20.2%"} positionTop={"-40%"} /> */}
-
-export default Grid;
